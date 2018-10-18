468ec70224ec5ddd28d2159999c76ecdf97b0202
Your Name <you@example.com>
2018-10-18 19:10:26 +0200

    Added portfolio projects, responsive design.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,18 @@ const msg = document.querySelector('#content');
 msg.value = '';
 mail.addEventListener("click", (e) => {
     e.preventDefault();
-    const name = document.querySelector('#name');  
+    const name = document.querySelector('#name');
     const email = document.querySelector('#email');
     const valMsg = document.querySelector('.hide');
-    
-   if (name.value === "" || email.value === "" || msg.value === "") {
+
+    if (name.value === "" || email.value === "" || msg.value === "") {
         return valMsg.classList.add('show');
-   } else {
-       mail.classList.add("fly");
-       valMsg.classList.remove('show');
-       valMsg.classList.add('hide');
-       name.value = "";
-       email.value = "";
-       msg.value = "";
-       setTimeout(() => mail.classList.remove("fly"), 5400);
-   }
+    }
+    mail.classList.add("fly");
+    valMsg.classList.remove('show');
+    valMsg.classList.add('hide');
+    name.value = "";
+    email.value = "";
+    msg.value = "";
+    setTimeout(() => mail.classList.remove("fly"), 5400);
 });
